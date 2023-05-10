ad04cf190764fff91ad8fa6cb9e3bc3c82d7eb86
dmitryvshivtsev <dmitryvshivtsev@gmail.com>
2023-05-10 20:35:29 +0300

    project done

--- friends/friends/settings.py
+++ friends/friends/settings.py
@@ -11,10 +11,13 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# .env
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -25,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['vshivtsev.site']
+ALLOWED_HOSTS = ['vshivtsev.site', '127.0.0.1']
 
 
 # Application definition
